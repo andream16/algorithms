426b5e08df00b06468ea356482ef0e008d87f29d
Andrea Medda <andrea.medda.uni@gmail.com>
2020-04-03 11:49:01 +0100

    Update ilovelanceandjanice.go

--- dynamic_programming/ilovelanceandjanice/ilovelanceandjanice.go
+++ dynamic_programming/ilovelanceandjanice/ilovelanceandjanice.go
@@ -1,20 +1,12 @@
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
 
 const (
 	alphabet = "abcdefghijklmnopqrstuvwxyz"
 	// a is 97, z is 122
 	maxRune = 122
 )
-
-func main() {
-	// "wrw blf hvv ozhg mrtsg'h vkrhlwv?" -> "did you see last night's episode?"
-	// "Yvzs! I xzm'g yvorvev Lzmxv olhg srh qly zg gsv xlolmb!!" -> "Yeah! I can't believe Lance lost his job at the colony!!"
-	fmt.Println(solution("Yvzs! I xzm'g yvorvev Lzmxv olhg srh qly zg gsv xlolmb!!"))
-}
 
 func solution(s string) string {
 	message := make([]rune, len(s))
