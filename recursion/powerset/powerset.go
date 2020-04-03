88c5d3c2a9738c1d1ddca934163f31d13a4ff23e
Andrea Medda <andrea.medda.uni@gmail.com>
2020-04-03 11:37:37 +0100

    Create powerset.go

---
+++ recursion/powerset/powerset.go
@@ -0,0 +1,13 @@
+func subsets(nums []int) [][]int {
+    return helper(nums, 0, [][]int{{}})
+}
+
+func helper(nums []int, i int, sets [][]int) [][]int {
+    if i == len(nums) {
+        return sets
+    }
+    for _, s := range sets {
+        sets = append(sets, append([]int{nums[i]}, s...))
+    }
+    return helper(nums, i+1, sets)
+}
