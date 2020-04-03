d391ff9b663340c7265adb7f7c24d7b228c87e28
Andrea Medda <andrea.medda.uni@gmail.com>
2020-04-03 06:24:31 +0100

    Create flattenbst.go

---
+++ trees_and_graphs/flattenbst.go
@@ -0,0 +1,39 @@
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+
+type LinkedList []*TreeNode
+
+func (l *LinkedList) Add(n *TreeNode) {
+    *l = append(*l, n)
+}
+
+func flatten(root *TreeNode) {
+    l := &LinkedList{}
+    preOrderTraversal(root, l)
+    
+    if ln := len(*l); ln <=1 {
+        return
+    }
+    
+    for _, n := range *l {
+        root.Left = nil
+        root.Right = n
+        root = root.Right
+    }
+}
+
+func preOrderTraversal(n *TreeNode, l *LinkedList) {
+    if n == nil {
+        return
+    }
+    l.Add(n)
+    preOrderTraversal(n.Left, l)
+    preOrderTraversal(n.Right, l)
+    return
+}
