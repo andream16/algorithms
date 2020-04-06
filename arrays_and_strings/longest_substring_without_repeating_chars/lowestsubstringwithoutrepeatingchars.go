897c9fcf459a48ddbc0d83fe13e7ba3a4b42661a
andrea <andrea.medda@imaginecurve.com>
2020-04-06 08:37:50 +0100

    renaming and adding exercise

---
+++ arrays_and_strings/longest_substring_without_repeating_chars/lowestsubstringwithoutrepeatingchars.go
@@ -0,0 +1,37 @@
+func lengthOfLongestSubstring(s string) int {
+
+	ls := len(s)
+
+	switch {
+	case s == "":
+		return 0
+	case ls == 1:
+		return 1
+	}
+
+	var (
+		w string
+		l int
+	)
+
+	for i, c := range s {
+		cs := string(c)
+		if i == 0 {
+			w += cs
+			continue
+		}
+		idx := strings.Index(w, cs)
+		if idx == -1 {
+			w += cs
+			if i != ls-1 {
+				continue
+			}
+		}
+		lw := len(w)
+		if lw > l {
+			l = lw
+		}
+		w = string(w[idx+1:] + cs)
+	}
+	return l
+}
