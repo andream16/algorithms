71bfd4cdae7f10e0c844b4b3abc4e618ba1798d1
unknown <andrea.medda.uni@gmail.com>
2020-04-11 10:50:08 +0100

    Adding prod

---
+++ arrays_and_strings/product_of_array_except_self/product.go
@@ -0,0 +1,33 @@
+package main
+
+import "fmt"
+
+// O(n) + O(n) -> O(n)
+func productExceptSelf(nums []int) []int {
+
+	var (
+		l        = len(nums)
+		right    = 1
+		products = make([]int, l)
+	)
+
+	// We know that the first value has to be 1.
+	products[0] = 1
+
+	// Initialise with products of numbers on the left of the current one.
+	for i := 1; i < len(nums); i++ {
+		products[i] = nums[i-1] * products[i-1]
+	}
+
+	// Finalise the result by multiplying what's left on current number's right.
+	for i := l - 1; i >= 0; i-- {
+		products[i] = products[i] * right
+		right *= nums[i]
+	}
+
+	return products
+}
+
+func main() {
+	fmt.Println(productExceptSelf([]int{1, 2, 3, 4}))
+}
