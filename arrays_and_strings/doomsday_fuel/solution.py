73e56671245a4430343ae2036a0747ee9804bfac
andrea <andrea.medda@imaginecurve.com>
2020-04-17 14:04:25 +0100

    Got basic case

--- arrays_and_strings/doomsday_fuel/solution.py
+++ arrays_and_strings/doomsday_fuel/solution.py
@@ -1,3 +1,7 @@
+from fractions import Fraction, gcd
+from functools import reduce
+
+
 def solution(m):
     if len(m) == 0 or len(m[0]) == 0:
         return []
@@ -5,21 +9,45 @@ def solution(m):
     num_absorbing_states = 0
 
     for i in range(0, len(m)):
+        sum_row = 0
         if is_absorbing_state(m[i]):
             m[i][i] = 1
             num_absorbing_states += 1
+        else:
+            for j in range(0, len(m[i])):
+                sum_row += m[i][j]
+            for j in range(0, len(m[i])):
+                if m[i][j] != 0:
+                    m[i][j] = float(m[i][j]) / float(sum_row)
 
     i = get_sub_matrix(m, len(m)-num_absorbing_states, num_absorbing_states-1)
     q = get_sub_matrix(m, 0, len(m)-num_absorbing_states-1)
 
     iq = sub(i, q)
+
     f = get_f(iq)
 
-    fr = product(f, get_sub_matrix(m, len(m)-num_absorbing_states-1, len(m)))
+    r = sub_matrix(m, 0, len(m)-num_absorbing_states, len(m)-num_absorbing_states-1, len(m))
 
-    print(fr[0])
+    fr = product(f, r)
 
-    # [0, 3, 2, 9, 14]
+    denominators = []
+    for n in fr[0]:
+        if n != 0:
+            denominators.append(Fraction(n).limit_denominator(100000000000).denominator)
+
+    denominator = reduce(lambda a, b: a*b // gcd(a, b), denominators)
+
+    res = []
+    for n in fr[0]:
+        if n == 0:
+            res.append(0)
+        else:
+            c = Fraction(n).limit_denominator(100000000000)
+            res.append(int(c.numerator * (denominator / c.denominator)))
+
+    res.append(denominator)
+    return res
 
 
 def get_sub_matrix(m, begin, end):
@@ -37,6 +65,17 @@ def get_sub_matrix(m, begin, end):
             cr += 1
             found = False
     return nm[:len(nm)-1]
+
+
+def sub_matrix(m, r1, c1, r2, c2):
+    sub_m = [[]]
+    for i in range(0, len(m)):
+        for j in range(0, len(m[i])):
+            if r1 <= i <= r2 and c1 <= j <= c2:
+                if len(sub_m[i]) == 0:
+                    sub_m.append([])
+                sub_m[i].append(m[i][j])
+    return sub_m[:len(sub_m)-1]
 
 
 def is_absorbing_state(nums):
@@ -57,24 +96,49 @@ def sub(i, q):
 
 
 def get_f(iq):
-    right, left = 1, 1
 
-    for i in range(0, len(iq)):
-        right *= iq[i][i]
-        left *= iq[len(iq)-i-1][i]
+    det = 1 / get_determinant(iq)
 
-    det = right - left
-    trasp = [[iq[j][i] for j in range(len(iq))] for i in range(len(iq[0]))]
+    trasp = traspose(iq)
 
     for i in range(0, len(trasp)):
         for j in range(0, len(trasp[i])):
-            trasp[i][j] = 1 / det * trasp[i][j]
+            trasp[i][j] = det * trasp[i][j]
 
     return trasp
+
+
+def traspose(m):
+    trasp = []
+    if len(m) == 2 and len(m[0]) == 2:
+        trasp = [[m[1][1], -m[0][1]], [-m[1][0], m[0][0]]]
+    return trasp
+
+
+def get_matrix_minor(m, i, j):
+    return [row[:j] + row[j+1:] for row in (m[:i]+m[i+1:])]
+
+
+def get_determinant(m):
+    if len(m) == 2:
+        return m[0][0]*m[1][1]-m[0][1]*m[1][0]
+
+    determinant = 0
+    for c in range(len(m)):
+        determinant += ((-1)**c)*m[0][c]*get_determinant(get_matrix_minor(m, 0, c))
+    return determinant
 
 
 def product(m1, m2):
     return [[sum(x * y for x, y in zip(m1_r, m2_c)) for m2_c in zip(*m2)] for m1_r in m1]
 
 
-solution([[0, 1, 0, 0, 0, 1], [4, 0, 0, 3, 2, 0], [0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0]])
+def main():
+    # [0, 3, 2, 9, 14]
+    print(solution([[0, 1, 0, 0, 0, 1], [4, 0, 0, 3, 2, 0], [0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0]]))
+    # [7, 6, 8, 21]
+    print(solution([[0, 2, 1, 0, 0], [0, 0, 0, 3, 4], [0, 0, 0, 0, 0], [0, 0, 0, 0,0], [0, 0, 0, 0, 0]]))
+
+
+if __name__ == "__main__":
+    main()
