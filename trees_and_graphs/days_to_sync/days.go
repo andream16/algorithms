b7482155437b81455feec58f53e848ded9364393
andrea <andrea.medda@imaginecurve.com>
2020-05-12 14:41:30 +0100

    improve server

--- trees_and_graphs/days_to_sync/days.go
+++ trees_and_graphs/days_to_sync/days.go
@@ -1,118 +1,81 @@
 package main
 
-import (
-	"fmt"
-	"math/rand"
-)
+import "fmt"
 
-type matrix struct {
-	nRows    int
-	nColumns int
-	values   [][]int
-}
+func daysToSync(m [][]int) int {
 
-func (m matrix) Print() {
-	for i := 0; i < m.nRows; i++ {
-		for j := 0; j < m.nColumns; j++ {
-			fmt.Printf("%d ", m.values[i][j])
-		}
-		fmt.Println("")
-	}
-}
+	const (
+		notSynced = iota
+		synced
+	)
 
-func newMatrix(nRows, nColumns int) matrix {
-	m := matrix{
-		nRows:    nRows,
-		nColumns: nColumns,
-		values:   make([][]int, nRows),
-	}
-	for i := 0; i < nRows; i++ {
-		m.values[i] = make([]int, nColumns)
-		for j := 0; j < nColumns; j++ {
-			m.values[i][j] = rand.Intn(2)
-		}
-	}
-	return m
-}
+	var (
+		days = 0
+		dirty = false
+	)
 
-func (m matrix) canBySynced(r, c int) bool {
-	if m.values[r][c] == 1 {
-		return false
-	}
-	switch r {
-	case 0:
-		switch c {
-		case 0:
-			return m.values[r+1][c] == 1 || m.values[r][c+1] == 1
-		case m.nColumns - 1:
-			return m.values[r][c-1] == 1 || m.values[r+1][c] == 1
-		default:
-			return m.values[r][c-1] == 1 || m.values[r][c+1] == 1 || m.values[r+1][c] == 1
-		}
-	case m.nRows - 1:
-		switch c {
-		case 0:
-			return m.values[r-1][c] == 1 || m.values[r][c+1] == 1
-		case m.nColumns - 1:
-			return m.values[r][c-1] == 1 || m.values[r-1][c] == 1
-		default:
-			return m.values[r][c-1] == 1 || m.values[r][c+1] == 1 || m.values[r-1][c] == 1
-		}
-	}
-	switch c {
-	case 0:
-		switch r {
-		case 0:
-			return m.values[r+1][0] == 1 || m.values[r][c+1] == 1
-		case m.nRows - 1:
-			return m.values[r][c] == 1 || m.values[r][c+1] == 1
-		default:
-			return m.values[r-1][c] == 1 || m.values[r][c+1] == 1 || m.values[r+1][c] == 1
-		}
-	case m.nRows - 1:
-		switch c {
-		case 0:
-			return m.values[r][c-1] == 1 || m.values[r+1][c] == 1
-		case m.nColumns - 1:
-			return m.values[r][c-1] == 1 || m.values[r-1][c] == 1
-		default:
-			return m.values[r-1][c] == 1 || m.values[r][c-1] == 1 || m.values[r+1][c] == 1
-		}
-	}
-	return m.values[r][c-1] == 1 || m.values[r-1][c] == 1 || m.values[r][c+1] == 1 || m.values[r+1][c] == 1
-}
-
-func (m matrix) daysToSync(days int) int {
-	fmt.Println(days)
-	m.Print()
-	dirty := false
-	v := make([][]int, m.nRows)
-	for i := 0; i < m.nRows; i++ {
-		v[i] = make([]int, m.nColumns)
-		for j := 0; j < m.nColumns; j++ {
-			if m.canBySynced(i, j) {
-				v[i][j] = 1
-				dirty = true
-				continue
+	for !dirty {
+		dirty = true
+		hm := deepCopy(m)
+		for i:=0; i<len(m); i++ {
+			for j:=0; j<len(m[i]); j++ {
+				if m[i][j] == synced {
+					continue
+				}
+				dirty = false
+				for _, mvs := range [][]int{{-1, 0}, {0,1}, {1, 0}, {0, -1}} {
+					currRow, currCol := i+mvs[0], j+mvs[1]
+					if currRow < 0 || currCol < 0 || currRow >= len(m) || currCol >= len(m[i]) || m[currRow][currCol] == notSynced {
+						continue
+					}
+					hm[i][j] = synced
+					break
+				}
 			}
-			v[i][j] = 0
+		}
+		if !dirty {
+			days++
+		}
+		m = hm
+	}
+
+	return days
+
+}
+
+func deepCopy(m [][]int) [][]int {
+	nm := make([][]int, len(m))
+	for i:=0; i<len(m); i++ {
+		nm[i] = make([]int, len(m[i]))
+		for j:=0; j<len(m[i]); j++ {
+			nm[i][j] = m[i][j]
 		}
 	}
-	if dirty {
-		m.values = v
-		days++
-		return m.daysToSync(days)
-	}
-	return days
+	return nm
 }
 
 // Given a matrix of 0s and 1s, where 0 means that a server is not synced and 1 means that it is:
 // return the number of days that it would take to sync all the servers not synced.
 // A synced server can sync any non synced server if it's adjacent (up, down, left, right).
 func main() {
-
-	m := newMatrix(16, 16)
-
-	m.daysToSync(0)
-
+	fmt.Println(daysToSync([][]int{
+		{1, 0, 0, 0},
+		{0, 0, 0, 0},
+		{0, 0, 0, 0},
+		{0, 0, 0, 0},
+	})) // 6
+	fmt.Println(daysToSync([][]int{
+		{1, 0, 0, 0},
+		{0, 0, 0, 0},
+		{0, 0, 0, 0},
+		{0, 0, 0, 1},
+	})) // 3
+	fmt.Println(daysToSync([][]int{
+		{1},
+	})) // 0
+	fmt.Println(daysToSync([][]int{
+		{1, 1, 1},
+		{1, 1, 1},
+		{1, 1, 1},
+	})) // 0
 }
