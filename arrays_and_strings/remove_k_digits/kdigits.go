5a1d159ce49ca5b6dee980cb621fe6dd9d5b6fa8
unknown <andrea.medda.uni@gmail.com>
2020-05-14 06:48:11 +0100

    Done witeh remove k digits

--- arrays_and_strings/remove_k_digits/kdigits.go
+++ arrays_and_strings/remove_k_digits/kdigits.go
@@ -1,25 +1,71 @@
 package kdigits
 
-// TODO CHECK THIS
+import "strings"
+
+// We remove the number that has bigger magnitude on the left of the current one as it will lead to a smaller number:
+// arr = [1,2,3,4,5,2,6,4], k = 4. We keep piling up until we find a smaller number on the right. In that case, we
+// remove from the stack until the condition is valid and k > 0. A stack facilitates this. If we have other k digits left
+// we simply remove them from the end of the result. We also need to remove trailing zeros from results like "002" -> "2".
+//
+// T: O(n+k) + O(n) + O(n) -> O(n)
+// Time Complexity is O(n) because, even if we have a nested loop, we know that we are going to loop a max of k times
+// globally and since 0 < k <= n the complexity of this is bound to 2N.
+//
+// S: O(n)
 func removeKdigits(num string, k int) string {
 	if k >= len(num) {
 		return "0"
 	}
 
-	for ; k > 0; k-- {
-		for i := 0; i < len(num)-1; i++ {
-			if num[i] > num[i+1] {
-				if i+1 != len(num)-1 && num[i+1] == '0' {
-					num = num[:i] + num[i+2:len(num)]
-				} else {
-					num = num[:i] + num[i+1:len(num)]
-				}
-				break
-			} else if num[i] <= num[i+1] {
-				num = num[:i+1] + num[i+2:len(num)]
-			}
+	s := &stack{}
+
+	for _, c := range num {
+		for k > 0 && !s.empty() && int(s.peak()-'0') > int(c-'0') {
+			s.pop()
+			k--
 		}
+		s.push(c)
 	}
 
-	return num
+	// remove extra digits if k>0.
+	str := s.toString()[:len(s.nums)-k]
+
+	// removing trailing zeros
+	return removeZeros(str)
+
+}
+
+type stack struct {
+	nums []rune
+}
+
+func (s *stack) push(n rune) {
+	s.nums = append(s.nums, n)
+}
+
+func (s *stack) pop() {
+	s.nums = s.nums[:len(s.nums)-1]
+}
+
+func (s *stack) peak() rune {
+	return s.nums[len(s.nums)-1]
+}
+
+func (s *stack) empty() bool {
+	return len(s.nums) == 0
+}
+
+func (s *stack) toString() string {
+	var builder strings.Builder
+	for _, n := range s.nums {
+		builder.WriteRune(n)
+	}
+	return builder.String()
+}
+
+func removeZeros(s string) string {
+	if len(s) > 1 && s[0] == '0' {
+		return removeZeros(s[1:])
+	}
+	return s
 }
--- arrays_and_strings/remove_k_digits/kdigits_test.go
+++ arrays_and_strings/remove_k_digits/kdigits_test.go
@@ -3,29 +3,20 @@ package kdigits
 import "testing"
 
 func TestRemoveKDigits(t *testing.T) {
-	t.Run("it should return 1219", func(t *testing.T) {
-		if num := removeKdigits("1432219", 3); num != "1219" {
-			t.Fatalf("expected 1219, got %s", num)
+	for _, testCase := range []struct {
+		givenNum string
+		givenK   int
+		expected string
+	}{
+		{givenNum: "12345264", givenK: 4, expected: "1224"},
+		{givenNum: "1432219", givenK: 3, expected: "1219"},
+		{givenNum: "10200", givenK: 1, expected: "200"},
+		{givenNum: "10", givenK: 2, expected: "0"},
+		{givenNum: "10", givenK: 1, expected: "0"},
+		{givenNum: "112", givenK: 1, expected: "11"},
+	} {
+		if res := removeKdigits(testCase.givenNum, testCase.givenK); res != testCase.expected {
+			t.Fatalf("expected %q, got %q", testCase.expected, res)
 		}
-	})
-	t.Run("it should return 200", func(t *testing.T) {
-		if num := removeKdigits("10200", 1); num != "200" {
-			t.Fatalf("expected 200, got %s", num)
-		}
-	})
-	t.Run("it should return 0", func(t *testing.T) {
-		if num := removeKdigits("10", 2); num != "0" {
-			t.Fatalf("expected 0, got %s", num)
-		}
-	})
-	t.Run("it should return 0", func(t *testing.T) {
-		if num := removeKdigits("10", 1); num != "0" {
-			t.Fatalf("expected 0, got %s", num)
-		}
-	})
-	t.Run("it should return 11", func(t *testing.T) {
-		if num := removeKdigits("112", 1); num != "11" {
-			t.Fatalf("expected 11, got %s", num)
-		}
-	})
+	}
 }
