23598a1f5ec34cc8b2c7dc5903f3a2efc1dc4b18
unknown <andrea.medda.uni@gmail.com>
2020-05-14 06:55:39 +0100

    Improved remove k digits

--- arrays_and_strings/remove_k_digits/kdigits.go
+++ arrays_and_strings/remove_k_digits/kdigits.go
@@ -1,13 +1,11 @@
 package kdigits
 
-import "strings"
-
 // We remove the number that has bigger magnitude on the left of the current one as it will lead to a smaller number:
 // arr = [1,2,3,4,5,2,6,4], k = 4. We keep piling up until we find a smaller number on the right. In that case, we
 // remove from the stack until the condition is valid and k > 0. A stack facilitates this. If we have other k digits left
 // we simply remove them from the end of the result. We also need to remove trailing zeros from results like "002" -> "2".
 //
-// T: O(n+k) + O(n) + O(n) -> O(n)
+// T: O(n+k) + O(n) -> O(n)
 // Time Complexity is O(n) because, even if we have a nested loop, we know that we are going to loop a max of k times
 // globally and since 0 < k <= n the complexity of this is bound to 2N.
 //
@@ -17,50 +15,23 @@ func removeKdigits(num string, k int) string {
 		return "0"
 	}
 
-	s := &stack{}
+	var str string
 
+	// we are using str like a stack.
 	for _, c := range num {
-		for k > 0 && !s.empty() && int(s.peak()-'0') > int(c-'0') {
-			s.pop()
+		for k > 0 && str != "" && int(str[len(str)-1]-'0') > int(c-'0') {
+			str = str[:len(str)-1]
 			k--
 		}
-		s.push(c)
+		str += string(c)
 	}
 
 	// remove extra digits if k>0.
-	str := s.toString()[:len(s.nums)-k]
+	str = str[:len(str)-k]
 
 	// removing trailing zeros
 	return removeZeros(str)
 
-}
-
-type stack struct {
-	nums []rune
-}
-
-func (s *stack) push(n rune) {
-	s.nums = append(s.nums, n)
-}
-
-func (s *stack) pop() {
-	s.nums = s.nums[:len(s.nums)-1]
-}
-
-func (s *stack) peak() rune {
-	return s.nums[len(s.nums)-1]
-}
-
-func (s *stack) empty() bool {
-	return len(s.nums) == 0
-}
-
-func (s *stack) toString() string {
-	var builder strings.Builder
-	for _, n := range s.nums {
-		builder.WriteRune(n)
-	}
-	return builder.String()
 }
 
 func removeZeros(s string) string {
