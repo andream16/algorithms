f631ed6e383bc42698b2e741e435be32945fda90
andrea <andrea.medda@imaginecurve.com>
2020-06-16 10:51:09 +0100

    Adding my version of perms

--- recursion/permutations/perm.go
+++ recursion/permutations/perm.go
@@ -1,25 +1,31 @@
 package permutations
 
-// T: O(N!)
-// S: O(N!)
+// To solve this problem we can use recursion.
+// If we think about it, a combination is valid when it has the same length of the input.
+// So, all we need to do is, have a concept of current combination (left) and left items (right)
+// that we need to analyse every time.
+// We start with current: [] and left: [1, 2, 3]
+// Then we would have current: [1] and left: [2, 3], current: [2] and left: [1, 3], current: [3] and left: [1, 2]
+// and we would keep iterating until the base condition is met.
+//
+// T: O(n!)
+// S: O(n!)
 func permute(nums []int) [][]int {
-	var perms [][]int
-	if len(nums) == 1 {
-		perms = append(perms, nums)
-		return perms
-	}
-	backtrack(0, &nums, &perms)
-	return perms
+	var res [][]int
+	permuteRec([]int{}, nums, &res)
+	return res
 }
 
-// by moving the index & swapping current with next and back, we get all permutations starting with a given index first.
-func backtrack(first int, nums *[]int, res *[][]int) {
-	if first == len(*nums) {
-		*res = append(*res, append([]int{}, *(nums)...))
+func permuteRec(currComb, left []int, res *[][]int) {
+	if 0 == len(left) {
+		*res = append(*res, currComb)
+		return
 	}
-	for i := first; i < len(*nums); i++ {
-		(*nums)[first], (*nums)[i] = (*nums)[i], (*nums)[first]
-		backtrack(first+1, nums, res)
-		(*nums)[first], (*nums)[i] = (*nums)[i], (*nums)[first]
+	for idx, l := range left {
+		permuteRec(
+			append(currComb, l),
+			append(append([]int{}, left[:idx]...), left[idx+1:]...),
+			res,
+		)
 	}
 }
